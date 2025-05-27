359a87aa6b1742aafd9afeb0f83f547e1e0332a4
sakini908 <nicolesakini@gmail.com>
2025-05-28 00:10:42 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,1 +1,11 @@
+// Get command-line arguments
+const args = process.argv.slice(2);
 
+// Check the number of arguments and print the corresponding message
+if (args.length === 0) {
+    console.log("No argument");
+} else if (args.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
