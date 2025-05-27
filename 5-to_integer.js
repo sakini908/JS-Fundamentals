8adffc7dbe9964a46a50df7130581267dfd66888
sakini908 <nicolesakini@gmail.com>
2025-05-28 00:16:02 +0300

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,1 +1,12 @@
+// Get the first argument from the command line
+const arg = process.argv[2];
 
+// Convert argument to an integer
+const num = parseInt(arg, 10);
+
+// Check if the conversion is a valid number
+if (!isNaN(num)) {
+    console.log(`My number: ${num}`);
+} else {
+    console.log("Not a number");
+}
