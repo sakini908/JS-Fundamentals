7c0e2ee98c558d30582f32b8eec01a21899edafb
sakini908 <nicolesakini@gmail.com>
2025-05-28 00:12:34 +0300

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,1 +1,5 @@
+// Get command-line arguments, avoiding the use of length
+const args = process.argv.slice(2);
 
+// Check if an argument is present and print the appropriate message
+console.log(args[0] || "No argument");
