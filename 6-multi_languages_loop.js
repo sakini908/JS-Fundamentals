7a4e408b60095244544416ee796d3e3cfa653844
sakini908 <nicolesakini@gmail.com>
2025-05-28 00:17:22 +0300

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,1 +1,5 @@
+// Define an array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
+// Use a loop to print all elements with a single console.log
+for (const line of languages) console.log(line);
