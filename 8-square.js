05910dc81219d23633604857177fdd7ad9b7d454
sakini908 <nicolesakini@gmail.com>
2025-05-28 08:38:46 +0300

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,1 +1,15 @@
+#!/usr/bin/node
 
+const size = Number(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else if (size > 0) {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
