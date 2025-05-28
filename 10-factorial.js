62c4d3ecf830dfa42e52c56962575ce8338dee97
sakini908 <nicolesakini@gmail.com>
2025-05-28 08:41:05 +0300

    Update 10-factorial.js

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,1 +1,18 @@
+#!/usr/bin/node
 
+function factorial(n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  if (n < 0) {
+    return 1; // Factorial is not typically defined for negative numbers. Returning 1 as per example for NaN.
+  }
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = Number(process.argv[2]);
+
+console.log(factorial(num));
