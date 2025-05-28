a132abac9aaf01506f6725abaf09f1ea56c76b60
sakini908 <nicolesakini@gmail.com>
2025-05-28 08:37:22 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,1 +1,13 @@
+#!/usr/bin/node
 
+const x = Number(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+}
